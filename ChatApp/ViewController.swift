e3ad93d872eae7cd06daacdab63b826b18ae8871
aHmEdaBoElSoOd33 <20912018100378@fci.zu.edu.eg>
2024-01-30 15:42:59 +0200

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 2B on 30/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
