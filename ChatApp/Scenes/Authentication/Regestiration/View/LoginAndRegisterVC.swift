fa0419a4aeacd76cfb32cb0bd111563190c7777f
aHmEdaBoElSoOd33 <20912018100378@fci.zu.edu.eg>
2024-02-12 18:05:11 +0200

    Register Form Design

--- ChatApp/Scenes/Authentication/Regestiration/View/LoginAndRegisterVC.swift
+++ ChatApp/Scenes/Authentication/Regestiration/View/LoginAndRegisterVC.swift
@@ -9,21 +9,46 @@ import UIKit
 
 class LoginAndRegisterVC: UIViewController {
 
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var switchFormBtn: UIButton!
+    @IBOutlet weak var RegisterStatusLbl: UILabel!
+    @IBOutlet weak var RegisterBtn: UIButton!
+    @IBOutlet weak var resendEmailBtn: UIButton!
+    @IBOutlet weak var confirmPasswordTxtField: UITextField!
+    @IBOutlet weak var emailTitle: UILabel!
+    @IBOutlet weak var forgetPasswordBtn: UIButton!
+    @IBOutlet weak var confirmPasswordTitle: UILabel!
+    @IBOutlet weak var passwordTxtFeild: UITextField!
+    @IBOutlet weak var passwordTitle: UILabel!
+    @IBOutlet weak var emailTxtFeild: UITextField!
+    
+    //MARK: - Variables
+    
+    //MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Methods
+    
+    //MARK: - IBActions
+    
+    @IBAction func forgetPasswordBtn(_ sender: UIButton) {
+        print("Forget pass")
     }
-    */
-
+    
+    @IBAction func resendEmailBtn(_ sender: UIButton) {
+        print("resendEmailBtn")
+    }
+    
+    @IBAction func RegisterBtn(_ sender: UIButton) {
+        print("RegisterBtn")
+    }
+    
+    @IBAction func switchFormBtn(_ sender: UIButton) {
+        print("switchFormBtn")
+    }
 }
